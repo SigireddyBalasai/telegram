b7a4b61b8197caa80124218bf5ae6e9df0674ebb
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2022-10-23 17:41:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import aiohttp
+import asyncio
+import typing
+
+
+class app():
+  base_url = "https://api.telegram.org/bot"
+  self.token = token
+  def __init__(self):
+    self.token = None
+  def run(token):
+    self.token = token
+ class User():
+    def 
+    id = id : int
+    self.is_bot = Bot : boolean
+  
