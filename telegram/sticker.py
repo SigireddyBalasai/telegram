c6dddb3a1b53c49d78e1ee5eeafc1e0acf76255b
anu <anideepabhinay@gmail.com>
2022-11-03 10:51:22 +0530

    added ms sti

---
+++ telegram/sticker.py
@@ -0,0 +1,12 @@
+from PhotoSize import PhotoSize
+class Sticker:
+    def __init__(self):
+        self.file_id: str=None
+        self.file_unique_id: str=None
+        self.type: str=None
+        self.width: int=None
+        self.height: int=None
+        self.is_animated: bool=None
+        self.is_video: bool=None
+        self.thumb: PhotoSize=None
+        self.emoji: str=None
