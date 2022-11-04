c20e02fbf87bc5e88223fb5c896dd606ed02ed41
anu <anideepabhinay@gmail.com>
2022-11-04 10:19:15 +0530

    added ms sti

--- telegram/polloption.py
+++ telegram/polloption.py
@@ -1,8 +1,10 @@
 from App import content
+
+
 class PollOPtion:
     def __init__(self):
-        self.text: str=None
-        self.voter_count: str=None
+        self.text: str = None
+        self.voter_count: str = None
 
     def set(self, context):
         self.text: str = content(context, 'text')
