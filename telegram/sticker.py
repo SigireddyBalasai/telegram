d2ab00b18495f4c59ff3cf1cc45b64cd9341befe
anu <anideepabhinay@gmail.com>
2022-11-05 10:15:54 +0530

    added ms sti

--- telegram/sticker.py
+++ telegram/sticker.py
@@ -1,4 +1,6 @@
 from PhotoSize import PhotoSize
+from File import File
+from App import content
 class Sticker:
     def __init__(self):
         self.file_id: str=None
@@ -12,3 +14,16 @@ class Sticker:
         self.emoji: str=None
         self.set_name: str=None
         self.premium_animation: File =None
+
+    def set(self, context):
+        self.file_id: str = content(context, 'file_id')
+        self.file_unique_id: str = content(context, 'file_unique_id')
+        self.type: str = content(context, 'type')
+        self.width: int = content(context, 'width')
+        self.height: int = content(context, 'height')
+        self.is_animated: bool = content(context, 'is_animated')
+        self.is_video: bool = content(context, 'is_video')
+        self.thumb: PhotoSize = content(context, 'thumb')
+        self.emoji: str = content(context, 'emoji')
+        self.set_name: str = content(context, 'set_name')
+        self.premium_animation: File = File().set_data(context['premium_animation'])
